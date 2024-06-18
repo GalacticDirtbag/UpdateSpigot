9d181c9c607bc8bb899c75d7ca7f88f0c05e9689
GalacticDirtbag <51019817+GalacticDirtbag@users.noreply.github.com>
2024-06-18 02:24:12 -0500

    Update check_mc_update.sh
    -Added more configurable options
    -Added check to ensure server updates when Spigot achieves parity with vanilla Minecraft
    -Added code to remove trailing ".jar" from vanilla version strings
    -Commenting

--- check_mc_update.sh
+++ check_mc_update.sh
@@ -7,11 +7,28 @@ SPIGOT_VERSION_MANIFEST=($(curl -s "$SPIGOT_VERSION_MANIFEST_URL" |\
                            grep -o -E '[0-9]\.[0-9]\.[0-9]|[0-9]\.[0-9][0-9]\.[0-9]|[0-9]\.[0-9][0-9]\.[0-9][0-9]' |\
                            sort -t . -k 1,1n -k 2,2n -k 3,3n))
 LATEST_SPIGOT_VERSION=$(echo "${SPIGOT_VERSION_MANIFEST[-1]}")
+IS_SPIGOT=$(cat prevBuild.txt 2> /dev/null)
 
-pushd /srv/minecraft/buildTools
-if [ -a ../minecraft_server.jar ];
+# User options:
+# MC_SERVER_DIR = full path to the directory of your minecraft server
+MC_SERVER_DIR=/srv/minecraft
+# BUILD_TOOLS_DIR = full path to your BuildTools.jar
+BUILD_TOOLS_DIR=/srv/minecraft/buildTools
+# VANILLA_UPDATE = download vanilla Minecraft server if no Spigot parity
+VANILLA_UPDATE=true
+
+
+# Change working directory
+pushd $BUILD_TOOLS_DIR
+
+# Check if server jar exists
+if [ -a $MC_SERVER_DIR/minecraft_server.jar ];
 then
-  CURRENT_SPIGOT_VERSION=$(unzip -p ../minecraft_server.jar META-INF/versions.list | cut -d'-' -f 2)
+  # Get current server version
+  CURRENT_SPIGOT_VERSION=$(unzip -p $MC_SERVER_DIR/minecraft_server.jar META-INF/versions.list | cut -d'-' -f 2 )
+
+  # (Parsing vanilla version strings requires removing the extension)
+  CURRENT_SPIGOT_VERSION=${CURRENT_SPIGOT_VERSION%.jar}
 else
   CURRENT_SPIGOT_VERSION=$(echo "NONE")
 fi
@@ -21,18 +38,39 @@ echo "The latest version of Spigot Minecraft Server is $LATEST_SPIGOT_VERSION."
 echo "You have version $CURRENT_SPIGOT_VERSION."
 echo
 
-if [ "$CURRENT_SPIGOT_VERSION" != "$LATEST_VANILLA_VERSION" ];
+# If server is not up to date or is vanilla
+if [ "$CURRENT_SPIGOT_VERSION" != "$LATEST_VANILLA_VERSION" ] || ([ "$LATEST_SPIGOT_VERSION" == "$LATEST_VANILLA_VERSION" ] && [ "$IS_SPIGOT" != "spigot" ]);
 then
-  echo "Looks like you need an update. Downloading now..."
+  echo "Looks like you need an update."
+  
+  # If Spigot is up to date, build the new version
   if [ "$LATEST_SPIGOT_VERSION" == "$LATEST_VANILLA_VERSION" ];
   then
-    java -jar BuildTools.jar --rev $LATEST_VANILLA_VERSION --output-dir .. --final-name minecraft_server.jar
+    echo "Downloading now..."
+
+    # Clear any existing build artifacts, as this can cause build errors
+    rm -R apache-maven-* BuildData Bukkit CraftBukkit Spigot work
+
+    # Build the new version of Spigot
+    java -jar BuildTools.jar --rev $LATEST_VANILLA_VERSION --output-dir $MC_SERVER_DIR --final-name minecraft_server.jar
+
+    # Tell script server jar is Spigot on next run
+    echo spigot > prevBuild.txt
   else
-    echo "Hmm...Looks like Spigot isn't up-to-date yet. Downloading vanilla..."
-    LATEST_VANILLA_MANIFEST=$(echo "$VANILLA_VERSION_MANIFEST" | jq -r --arg LATEST_VANILLA_VERSION "$LATEST_VANILLA_VERSION" '.versions | .[] | select(.id==$LATEST_VANILLA_VERSION) | .url')
-    LATEST_VANILLA_MANIFEST_DATA=$(curl -s $LATEST_VANILLA_MANIFEST)
-    VANILLA_JAR=$(jq -rn --argjson url "$LATEST_VANILLA_MANIFEST_DATA" '$url.downloads.server.url')
-    curl -L $VANILLA_JAR -o ../minecraft_server.jar
+    echo "Hmm...Looks like Spigot isn't up-to-date yet."
+
+    if [ "$VANILLA_UPDATE" == "true" ];
+    then
+      echo "Downloading vanilla..."
+      # Determine the latest vanilla server version of Minecraft and download
+      LATEST_VANILLA_MANIFEST=$(echo "$VANILLA_VERSION_MANIFEST" | jq -r --arg LATEST_VANILLA_VERSION "$LATEST_VANILLA_VERSION" '.versions | .[] | select(.id==$LATEST_VANILLA_VERSION) | .url')
+      LATEST_VANILLA_MANIFEST_DATA=$(curl -s $LATEST_VANILLA_MANIFEST)
+      VANILLA_JAR=$(jq -rn --argjson url "$LATEST_VANILLA_MANIFEST_DATA" '$url.downloads.server.url')
+      curl -L $VANILLA_JAR -o $MC_SERVER_DIR/minecraft_server.jar
+
+      # Tell script server jar is vanilla on next run
+      echo vanilla > prevBuild.txt
+    fi
   fi
 else
   echo "Congrats! You're already up to date!"
